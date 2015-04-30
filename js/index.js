1320257c7f0249e06d2c2148fb21bf434e2f75d2
Xiaotao Liang <418xiaotao@163.com>
2015-04-30 15:28:50 +0800

    1. modify the names of icons 2. modify css of the button 3. modify the modal

--- js/index.js
+++ js/index.js
@@ -1,8 +1,26 @@
-function changeImage(imagePath)
-{
-	$(this).css("src", imagePath);
-	alert(imagePath);
-}
+$("#imageArticle").mouseover(function(){
+	$(this).attr("src", "icons/user_edit_article_active.png");
+});
+
+$("#imageArticle").mouseout(function(){
+	$(this).attr("src", "icons/user_edit_article.png");
+});
+
+$("#imageVideo").mouseover(function(){
+	$(this).attr("src", "icons/user_edit_video_active.png");
+});
+
+$("#imageVideo").mouseout(function(){
+	$(this).attr("src", "icons/user_edit_video.png");
+});
+
+$("#imagePhoto").mouseover(function(){
+	$(this).attr("src", "icons/user_edit_photo_active.png");
+});
+
+$("#imagePhoto").mouseout(function(){
+	$(this).attr("src", "icons/user_edit_photo.png");
+});
 
 $(".pane:odd").css("background-color", "#fbfbfb");
 
